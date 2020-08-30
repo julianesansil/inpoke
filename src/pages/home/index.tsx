96c123d0d9db6ed923a2aab866f0f296aa37e22f
Juliane Silva <jl.sansil@gmail.com>
2020-08-30 11:01:15 -0400

    feat: add pokemon list request

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,16 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect } from 'react';
+import { pokemonAPI } from '../../services/api/pokemon.api';
 
 const Home: FunctionComponent = () => {
+  const listPokemon = async () => {
+    const response = await pokemonAPI.listPokemon();
+    console.log(response.data);
+  };
+
+  useEffect(() => {
+    listPokemon();
+  });
+
   return <h1>InPoke</h1>;
 };
 
--- src/services/api/pokemon.api.ts
+++ src/services/api/pokemon.api.ts
@@ -1,1 +1,9 @@
-export {};
+import api from './base.api';
+
+const listPokemon = () => {
+  return api.get('pokemon');
+};
+
+export const pokemonAPI = {
+  listPokemon,
+};
