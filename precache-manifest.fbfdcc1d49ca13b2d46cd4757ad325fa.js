20e45bf99cc53bf49660b72bad6016931eb2c1f4
Juliane Silva <jl.sansil@gmail.com>
2020-08-31 11:01:34 -0400

    chore: build project

---
+++ precache-manifest.fbfdcc1d49ca13b2d46cd4757ad325fa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cbd5b5fa9e25d62ac11dba9a5eea01c",
+    "url": "/inpoke/index.html"
+  },
+  {
+    "revision": "57c0e48cbb18272dad08",
+    "url": "/inpoke/static/js/2.8ab4d1c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/inpoke/static/js/2.8ab4d1c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f64e1aec2d148dcb837c",
+    "url": "/inpoke/static/js/main.cbd1e044.chunk.js"
+  },
+  {
+    "revision": "2897380003687b082821",
+    "url": "/inpoke/static/js/runtime-main.3785f0c4.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/inpoke/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/inpoke/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
+    "url": "/inpoke/static/media/Pokemon-Solid.36d26dbc.ttf"
+  },
+  {
+    "revision": "6b4ffa55fdd7e087c6990685887572b4",
+    "url": "/inpoke/static/media/pokebola.6b4ffa55.svg"
+  }
+]);
